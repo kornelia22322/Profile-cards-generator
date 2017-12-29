e8b4d73178d6e4754956d5449573027196f5c4c1
korneliarohulko <korneliarohulko@gmail.com>
2017-12-29 03:45:53 +0100

    Minor fix

--- js/script.js
+++ js/script.js
@@ -1,44 +1,32 @@
 var db = [
-    {name:'James Butters',email:'James@gmail.com',age:15},
-    {name:'Mark Robinson',email:'Mark@gmail.com',age:23},
-    {name:'Lara Barbosa',email:'Lara@gmail.com',age:23}
+    {name:'James Butters', email:'James@gmail.com', age:15},
+    {name:'Mark Robinson', email:'Mark@gmail.com', age:23},
+    {name:'Lara Barbosa', email:'Lara@gmail.com', age:23}
 ];
 
 (function Avatars(db){
 	this.init = function(){
 		this.generateList();
+	}
 
 	this.generateList = function(){
 
-		var parent = document.querySelector('#parent_avatars');
+		var parent = document.querySelector('#parent_avatar');
 		var template = '';
 
 		for(var i = 0; i < db.length; i++){
-
-			var container = $("<div>");
-			container.addClass("col-sm-4");
-			var containerInner = $("<div");
-			containerInner.addClass("card");
-			container.append(containerInner);
-			var containerCardDelete = $("<div");
-			containerCardDelete.addClass("card-delete");
-			containerInner.append(containerCardDelete);
-
-			template += '<div class="col-sm-4">
-    						<div class="card">
-    							<div class="card-delete" data-card="">x</div>
-    							<div class="card-block">
-									<h3 class="card-title">Lara Barbosa</h3>
-									<p class="card-text"><strong>Email: </strong>lara@gmail.com</p>   
-									<p class="card-text"><strong>Age: </strong>25</p> 						
-    							</div>
-    						</div>
-    					</div>'
-		}
-
-			
-		}
-
+            template += '<div class="col-sm-4">';
+            template +=  '<div class="card">';
+            template +=     '<div class="card-delete" data-card="'+ i +'">x</div>';
+            template +=     '<div class="card-block">';
+            template +=         '<h3 class="card-title">'+ db[i].name +'</h3>';
+            template +=         '<p class="card-text"><strong>Email:</strong> '+ db[i].email +'</p>';
+            template +=         '<p class="card-text"><strong>Age:</strong> '+ db[i].age +'</p>';
+            template +=     '</div>';
+            template +=   '</div>';
+            template += '</div>';
+        }			
+        parent.insertAdjacentHTML('afterbegin', template);
 	};
 
 	this.init();
