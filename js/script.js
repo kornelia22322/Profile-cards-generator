6c6f40867559c894881bc338b9a7d944a6228da7
korneliarohulko <korneliarohulko@gmail.com>
2017-12-29 19:36:04 +0100

    Add jQuery to animate cards

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 var db = [
-    {name:'James Butters', email:'James@gmail.com', age:15},
-    {name:'Mark Robinson', email:'Mark@gmail.com', age:23},
-    {name:'Lara Barbosa', email:'Lara@gmail.com', age:23}
+    {name:'James Butters', email:'James@gmail.com', age: 15},
+    {name:'Mark Robinson', email:'Mark@gmail.com', age: 23},
+    {name:'Lara Barbosa', email:'Lara@gmail.com', age: 23},
+    {name:'Amy Lee', email:'amy@gmail.com', age: 28},
+    {name:'Amy Lee', email:'amy@gmail.com', age: 280}
 ];
 
 (function Avatars(db){
 	this.init = function(){
-		this.generateList(0);
-		this.enterUser();
+		this.generateList();
+		this.enterUser();		
 	}
 
-	this.generateList = function(j){
+	this.generateList = function(){
 
 		var parent = document.querySelector('#parent_avatar');
+		parent.innerHTML = '';
 		var template = '';
 
-		for(var i = j; i < db.length; i++){
+		for(var i = 0; i < db.length; i++){
 		    template += '<div class="col-sm-4">';
 		    template +=  '<div class="card">';
-		    template +=     '<div class="card-delete" data-card="'+ i +'">x</div>';
+		    template +=     '<div class="card-delete" data-card="'+i+'"'+ i +'"><i class="fa fa-times hid" aria-hidden="true"></i></div>';
 		    template +=     '<div class="card-block">';
 		    template +=         '<h3 class="card-title">'+ db[i].name +'</h3>';
 		    template +=         '<p class="card-text"><strong>Email:</strong> '+ db[i].email +'</p>';
@@ -26,8 +29,12 @@ var db = [
 		    template +=     '</div>';
 		    template +=   '</div>';
 		    template += '</div>';
+
+       		parent.insertAdjacentHTML('afterbegin', template);
+       		template = '';
+       		loadCards();
         }			
-        parent.insertAdjacentHTML('afterbegin', template);
+        deleteCard();  
 	};
 
 	this.enterUser = function(){
@@ -40,13 +47,12 @@ var db = [
 			var age = document.querySelector('#user_age').value;
 
 			var parent = document.querySelector('.form-container');
-
 			var elements = [name, email, age];
 
 			if(validateUser(elements)){
 				document.querySelector("#myForm").reset();
 				db.push({name: name, email: email, age: age});
-				this.generateList(db.length-1);
+				this.generateList();
 				if(emptyMsg) {
 					var tmp = document.querySelector('.emptyFormMsg');
 					tmp.innerHTML = '';
@@ -54,7 +60,7 @@ var db = [
 			} else if(!emptyMsg) {
 				var emptyFormMsg = '';
 				emptyFormMsg += '<div class="emptyFormMsg">'+ 'Form cannot be empty' + '</div>';
-				parent.insertAdjacentHTML('beforeend', emptyFormMsg);
+				parent.insertAdjacentHTML('beforeend', emptyFormMsg);				
 				emptyMsg = true;
 			}
 		}
@@ -72,7 +78,31 @@ var db = [
 			}
 		}
 		return true;
-	}
+	};
 
+	this.deleteCard = function(){
+		var button = document.querySelectorAll('.card-delete');	
+		function deleteThis(element){
+			var obj = element.getAttribute("data-card");
+			db.splice(obj, 1);
+			generateList();
+		}
+
+
+		for(var i = 0;i<button.length;i++){
+			button[i].addEventListener("click", function(e){
+				deleteThis(this);
+			});
+		}			
+	};
+	this.loadCards = function(){
+		  $( ".card" ).animate({
+		  	  height : "180px",
+		  	  width : "100%"
+		  }, 1000);
+		  setTimeout(function(){ 
+		  	$(".card-delete").css("display", "inline"); 
+		  }, 1000);		  
+	}
 	this.init();
 })(db);
