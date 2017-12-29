b1706dd4af82f0aaa612c70bd7d651ea4e40c125
korneliarohulko <korneliarohulko@gmail.com>
2017-12-29 03:47:20 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,18 +15,18 @@ var db = [
 		var template = '';
 
 		for(var i = 0; i < db.length; i++){
-            template += '<div class="col-sm-4">';
-            template +=  '<div class="card">';
-            template +=     '<div class="card-delete" data-card="'+ i +'">x</div>';
-            template +=     '<div class="card-block">';
-            template +=         '<h3 class="card-title">'+ db[i].name +'</h3>';
-            template +=         '<p class="card-text"><strong>Email:</strong> '+ db[i].email +'</p>';
-            template +=         '<p class="card-text"><strong>Age:</strong> '+ db[i].age +'</p>';
-            template +=     '</div>';
-            template +=   '</div>';
-            template += '</div>';
-        }			
-        parent.insertAdjacentHTML('afterbegin', template);
+		    template += '<div class="col-sm-4">';
+		    template +=  '<div class="card">';
+		    template +=     '<div class="card-delete" data-card="'+ i +'">x</div>';
+		    template +=     '<div class="card-block">';
+		    template +=         '<h3 class="card-title">'+ db[i].name +'</h3>';
+		    template +=         '<p class="card-text"><strong>Email:</strong> '+ db[i].email +'</p>';
+		    template +=         '<p class="card-text"><strong>Age:</strong> '+ db[i].age +'</p>';
+		    template +=     '</div>';
+		    template +=   '</div>';
+		    template += '</div>';
+        	}			
+        	parent.insertAdjacentHTML('afterbegin', template);
 	};
 
 	this.init();
